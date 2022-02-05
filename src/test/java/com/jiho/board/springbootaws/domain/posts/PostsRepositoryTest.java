c4f22653a30a030a006c4ee702e7ff4eba56e9a6
ParkJiHo <iks15174@daum.net>
2022-02-06 00:43:22 +0900

    PostsRepository test 추가

---
+++ src/test/java/com/jiho/board/springbootaws/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.jiho.board.springbootaws.domain.posts;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+        String author = "테스트 유저";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author(author).build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+        assertThat(posts.getAuthor()).isEqualTo(author);
+
+    }
+}
