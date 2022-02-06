cf8ced72ff3ab806810ade9bf402aa588888388a
ParkJiHo <iks15174@daum.net>
2022-02-06 20:36:17 +0900

    PostsSerivce & PostsSaveRequestDto 작성

--- src/main/java/com/jiho/board/springbootaws/service/posts/PostsService.java
+++ src/main/java/com/jiho/board/springbootaws/service/posts/PostsService.java
@@ -1,5 +1,21 @@
 package com.jiho.board.springbootaws.service.posts;
 
-public class PostsService {
+import javax.transaction.Transactional;
 
+import com.jiho.board.springbootaws.domain.posts.PostsRepository;
+import com.jiho.board.springbootaws.web.dto.PostsSaveRequestDto;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
--- src/main/java/com/jiho/board/springbootaws/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/jiho/board/springbootaws/web/dto/PostsSaveRequestDto.java
@@ -1,5 +1,26 @@
 package com.jiho.board.springbootaws.web.dto;
 
-public class PostsSaveRequestDto {
+import com.jiho.board.springbootaws.domain.posts.Posts;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder().title(title).content(content).author(author).build();
+    }
 }
