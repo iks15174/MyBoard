4d2941a4c5290e0c846542ba7fc657d4e6ceacb1
ParkJiHo <iks15174@daum.net>
2022-02-07 09:41:16 +0900

    post 수정 & 상제조회 controller 추가

--- src/main/java/com/jiho/board/springbootaws/web/PostsApiController.java
+++ src/main/java/com/jiho/board/springbootaws/web/PostsApiController.java
@@ -3,7 +3,10 @@ package com.jiho.board.springbootaws.web;
 import com.jiho.board.springbootaws.service.posts.PostsService;
 import com.jiho.board.springbootaws.web.dto.PostsSaveRequestDto;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +20,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
     }
 }
