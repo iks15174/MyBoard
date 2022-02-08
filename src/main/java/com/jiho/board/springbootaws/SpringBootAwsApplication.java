ec2280d3be7841a2896ebdc3ba65d8ed02aff4ca
ParkJiHo <iks15174@daum.net>
2022-02-08 09:45:16 +0900

    BaseTimeEntity 기능 추가

--- src/main/java/com/jiho/board/springbootaws/SpringBootAwsApplication.java
+++ src/main/java/com/jiho/board/springbootaws/SpringBootAwsApplication.java
@@ -2,7 +2,9 @@ package com.jiho.board.springbootaws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootAwsApplication {
 
---
+++ src/main/java/com/jiho/board/springbootaws/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.jiho.board.springbootaws.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdData;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
