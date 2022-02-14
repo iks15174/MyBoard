e9203eed0faed73d296367e07201e587cbb4e2bb
ParkJiHo <iks15174@daum.net>
2022-02-14 23:41:26 +0900

    h2-console 사용을 위한 security config 추가

--- src/main/java/com/jiho/board/springbootaws/config/SecurityConfig.java
+++ src/main/java/com/jiho/board/springbootaws/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.jiho.board.springbootaws.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -13,10 +16,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers("/api/v1/auth/**", "/h2-console/**", "/web-resources/**").permitAll()
                 .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
                 .antMatchers("/api/v1/**").hasRole("USER");
+
+        http.formLogin();
+        http.csrf().disable();
     }
 }
