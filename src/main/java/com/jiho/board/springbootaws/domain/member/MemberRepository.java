8955056f129c18601e39a95962f86d4fd0b24189
ParkJiHo <iks15174@daum.net>
2022-02-20 01:56:43 +0900

    MemberRepository findByEmail 추가

--- src/main/java/com/jiho/board/springbootaws/domain/member/MemberRepository.java
+++ src/main/java/com/jiho/board/springbootaws/domain/member/MemberRepository.java
@@ -1,7 +1,11 @@
 package com.jiho.board.springbootaws.domain.member;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
+    boolean existsByEmailAndSocial(String email, Social social);
 
+    Optional<Member> findByEmail(String username);
 }
