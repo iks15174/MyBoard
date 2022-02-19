875acef9cdc730c742654261c67174815a14f3e0
ParkJiHo <iks15174@daum.net>
2022-02-20 01:55:24 +0900

    jwt token 인증 필터 적용

--- src/main/java/com/jiho/board/springbootaws/config/SecurityConfig.java
+++ src/main/java/com/jiho/board/springbootaws/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.jiho.board.springbootaws.config;
 
+import com.jiho.board.springbootaws.config.filter.JwtFilter;
+import com.jiho.board.springbootaws.util.JWTUtil;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,13 +10,15 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
+    private final JWTUtil jwtUtil;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -31,5 +36,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin();
         http.csrf().disable();
+
+        http.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    JwtFilter jwtFilter() {
+        return new JwtFilter(jwtUtil);
     }
 }
