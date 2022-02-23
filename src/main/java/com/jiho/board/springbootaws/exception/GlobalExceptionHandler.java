7d26f5b7d517b7f8fcce4277c10757b960a692e1
ParkJiHo <iks15174@daum.net>
2022-02-23 14:43:07 +0900

    아이디 & 비밀번호 불일치 예외 핸들러 추가

--- src/main/java/com/jiho/board/springbootaws/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jiho/board/springbootaws/exception/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.jiho.board.springbootaws.exception;
 
 import com.jiho.board.springbootaws.exception.exceptions.CustomBasicException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,5 +15,11 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleCustomBasicException(CustomBasicException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
         return new ResponseEntity<>(errorResponse, e.changeToHttpStatus());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    protected ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e, 401);
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 }
