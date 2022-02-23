d6ef44886672e98b3c29883d47574fb17c878c29
ParkJiHo <iks15174@daum.net>
2022-02-23 14:44:35 +0900

    login & test controller 추가

--- src/main/java/com/jiho/board/springbootaws/web/MemberApiController.java
+++ src/main/java/com/jiho/board/springbootaws/web/MemberApiController.java
@@ -1,9 +1,18 @@
 package com.jiho.board.springbootaws.web;
 
+import javax.management.RuntimeErrorException;
+
 import com.jiho.board.springbootaws.service.member.MemberService;
+import com.jiho.board.springbootaws.service.member.dto.AuthMemberDto;
+import com.jiho.board.springbootaws.web.dto.member.LoginRequestDto;
 import com.jiho.board.springbootaws.web.dto.member.MemberResponseDto;
 import com.jiho.board.springbootaws.web.dto.member.MemberSaveRequestDto;
+import com.jiho.board.springbootaws.web.dto.member.TokenDto;
 
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +27,19 @@ public class MemberApiController {
 
     @PostMapping("/api/v1/auth/signup")
     public MemberResponseDto singup(@RequestBody MemberSaveRequestDto requestDto) {
-        System.out.println(requestDto.toString());
         return memberService.signup(requestDto);
+    }
+
+    @PostMapping("/api/v1/auth/login")
+    public TokenDto login(@RequestBody LoginRequestDto requestDto) throws Exception {
+        return memberService.login(requestDto);
+
+    }
+
+    @GetMapping("/api/v1/auth/test")
+    public void test() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(((AuthMemberDto) authentication.getPrincipal()).getUsername());
     }
 
 }
