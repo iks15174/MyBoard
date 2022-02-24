7c46e5c270bafdc147fcd6c4112ef5e661b9c9d8
ParkJiHo <iks15174@daum.net>
2022-02-24 15:58:24 +0900

    구글 로그인 구현

--- src/main/java/com/jiho/board/springbootaws/service/member/CustomOAuth2UserService.java
+++ src/main/java/com/jiho/board/springbootaws/service/member/CustomOAuth2UserService.java
@@ -1,24 +1,66 @@
 package com.jiho.board.springbootaws.service.member;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.jiho.board.springbootaws.domain.member.Member;
+import com.jiho.board.springbootaws.domain.member.MemberRepository;
+import com.jiho.board.springbootaws.domain.member.MemberRole;
+import com.jiho.board.springbootaws.service.member.dto.AuthMemberDto;
+import com.jiho.board.springbootaws.service.member.dto.OAuth2Attribute;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         String clientName = userRequest.getClientRegistration().getClientName();
-        System.out.println(clientName);
-
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        oAuth2User.getAttributes().forEach((k, v) -> {
-            System.out.println(k + ": " + v);
-        });
-        return oAuth2User;
+        OAuth2Attribute oAuth2Attribute = OAuth2Attribute.of(clientName, oAuth2User.getAttributes());
+        Member member = saveSocialMember(oAuth2Attribute);
+        AuthMemberDto authMemberDto = new AuthMemberDto(
+                member.getEmail(),
+                member.getPassword(),
+                member.getName(),
+                member.getSocial(),
+                member.getRoleSet().stream().map(
+                        role -> new SimpleGrantedAuthority(role.name())).collect(Collectors.toList()),
+                oAuth2User.getAttributes());
+
+        return authMemberDto;
+    }
+
+    private Member saveSocialMember(OAuth2Attribute oAuth2Attribute) {
+        Optional<Member> result = memberRepository
+                .findByEmailAndSocial(oAuth2Attribute.getEmail(), oAuth2Attribute.getSocial());
+
+        if (result.isPresent()) {
+            return result.get();
+        }
+
+        Member member = Member.builder()
+                .email(oAuth2Attribute.getEmail())
+                .password(passwordEncoder.encode("1234"))
+                .social(oAuth2Attribute.getSocial())
+                .name(oAuth2Attribute.getName())
+                .build();
+        member.addMemberRole(MemberRole.USER);
+        memberRepository.save(member);
+        return member;
     }
 
 }
--- src/main/java/com/jiho/board/springbootaws/service/member/dto/AuthMemberDto.java
+++ src/main/java/com/jiho/board/springbootaws/service/member/dto/AuthMemberDto.java
@@ -1,25 +1,39 @@
 package com.jiho.board.springbootaws.service.member.dto;
 
 import java.util.Collection;
+import java.util.Map;
 
 import com.jiho.board.springbootaws.domain.member.Social;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
-public class AuthMemberDto extends User {
+public class AuthMemberDto extends User implements OAuth2User {
     private String name;
     private Social social;
+    private Map<String, Object> attr;
+
+    public AuthMemberDto(String email, String password, String name, Social social,
+            Collection<? extends GrantedAuthority> authorities, Map<String, Object> attr) {
+        this(email, password, name, social, authorities);
+        this.attr = attr;
+    }
 
     public AuthMemberDto(String email, String password, String name, Social social,
             Collection<? extends GrantedAuthority> authorities) {
         super(email, password, authorities);
         this.name = name;
         this.social = social;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attr;
     }
 }
