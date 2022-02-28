9c4f9619c98df92d753ac775d44c577aa9dd53a0
ParkJiHo <iks15174@daum.net>
2022-03-01 00:40:54 +0900

    PostLists 불러오는 테스트 추가

--- src/test/java/com/jiho/board/springbootaws/web/PostsApiControllerTest.java
+++ src/test/java/com/jiho/board/springbootaws/web/PostsApiControllerTest.java
@@ -1,6 +1,7 @@
 package com.jiho.board.springbootaws.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jiho.board.springbootaws.domain.member.Member;
 import com.jiho.board.springbootaws.domain.member.MemberRepository;
 import com.jiho.board.springbootaws.domain.member.Social;
 import com.jiho.board.springbootaws.domain.posts.Posts;
@@ -26,119 +27,158 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
+import org.hamcrest.Matchers;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsApiControllerTest {
-    @LocalServerPort
-    private int port;
 
-    @Autowired
-    private WebApplicationContext context;
+        private Member testMember;
 
-    private MockMvc mvc;
+        private MockMvc mvc;
 
-    @Autowired
-    private PostsRepository postsRepository;
+        @LocalServerPort
+        private int port;
 
-    @Autowired
-    private MemberRepository memberRepository;
+        @Autowired
+        private WebApplicationContext context;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+        @Autowired
+        private PostsRepository postsRepository;
 
-    @AfterEach
-    public void tearDown() throws Exception {
-        postsRepository.deleteAll();
-        memberRepository.deleteAll();
-    }
+        @Autowired
+        private MemberRepository memberRepository;
 
-    @BeforeEach
-    public void setup() {
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
+        @Autowired
+        PasswordEncoder passwordEncoder;
 
-        memberRepository.save(MemberSaveRequestDto.builder()
-                .email("testUser@test.com")
-                .password("1234")
-                .name("testUser")
-                .social(Social.NoSocial).build()
-                .toEntity(passwordEncoder));
-    }
+        @AfterEach
+        public void tearDown() throws Exception {
+                postsRepository.deleteAll();
+                memberRepository.deleteAll();
+        }
 
-    @Test
-    @WithMockCustomUser
-    public void Posts_등록된다() throws Exception {
-        String title = "title";
-        String content = "content";
+        @BeforeEach
+        public void setup() {
+                mvc = MockMvcBuilders
+                                .webAppContextSetup(context)
+                                .apply(springSecurity())
+                                .build();
 
-        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
-                .title(title)
-                .content(content)
-                .build();
+                testMember = memberRepository.save(MemberSaveRequestDto.builder()
+                                .email("testUser@test.com")
+                                .password("1234")
+                                .name("testUser")
+                                .social(Social.NoSocial).build()
+                                .toEntity(passwordEncoder));
+        }
 
-        String url = "http://localhost:" + port + "/api/v1/posts";
+        @Test
+        public void PostsList_불러온다() throws Exception {
+                createPosts(11);
+                String url = "http://localhost:" + port + "/api/v1/posts";
 
-        mvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(requestDto)))
-                .andExpect(status().isOk());
+                mvc.perform(get(url))
+                                .andExpect(status().isOk())
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.pageInfo.totalPage").value(2))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.pageInfo.page").value(0))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.postsList.[0].title").value("title0"))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.postsList.[0].content").value("content0"))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.postsList.[9].title").value("title9"))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.postsList.[9].content").value("content9"))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.postsList.length()").value(10));
+        }
 
-        List<Posts> all = postsRepository.findAll();
-        assertThat(all.get(0).getTitle()).isEqualTo(title);
-        assertThat(all.get(0).getContent()).isEqualTo(content);
-    }
+        @Test
+        @WithMockCustomUser
+        public void Posts_등록된다() throws Exception {
+                String title = "title";
+                String content = "content";
 
-    @Test
-    @WithMockCustomUser
-    void Posts_수정된다() throws Exception {
-        Posts savedPosts = postsRepository
-                .save(Posts.builder().title("title").content("content").build());
-        Long updateId = savedPosts.getId();
-        String updatedTitle = "title-update";
-        String updatedContent = "content-update";
+                PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                                .title(title)
+                                .content(content)
+                                .build();
 
-        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
-                .title(updatedTitle)
-                .content(updatedContent)
-                .build();
+                String url = "http://localhost:" + port + "/api/v1/posts";
 
-        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+                mvc.perform(post(url)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                                .andExpect(status().isOk());
 
-        mvc.perform(put(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(requestDto)))
-                .andExpect(status().isOk());
+                List<Posts> all = postsRepository.findAll();
+                assertThat(all.get(0).getTitle()).isEqualTo(title);
+                assertThat(all.get(0).getContent()).isEqualTo(content);
+        }
 
-        List<Posts> all = postsRepository.findAll();
-        assertThat(all.get(0).getTitle()).isEqualTo(updatedTitle);
-        assertThat(all.get(0).getContent()).isEqualTo(updatedContent);
+        @Test
+        @WithMockCustomUser
+        void Posts_수정된다() throws Exception {
+                Posts savedPosts = postsRepository
+                                .save(Posts.builder().title("title").content("content").build());
+                Long updateId = savedPosts.getId();
+                String updatedTitle = "title-update";
+                String updatedContent = "content-update";
 
-    }
+                PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                                .title(updatedTitle)
+                                .content(updatedContent)
+                                .build();
 
-    @Test
-    public void BaseTimeEntity_등록() {
-        LocalDateTime now = LocalDateTime.of(2022, 2, 8, 0, 0, 0);
-        postsRepository.save(Posts.builder().title("title").content("content").build());
+                String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
 
-        List<Posts> postsList = postsRepository.findAll();
+                mvc.perform(put(url)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                                .andExpect(status().isOk());
 
-        Posts posts = postsList.get(0);
-        assertThat(posts.getCreatedData()).isAfter(now);
-        assertThat(posts.getModifiedDate()).isAfter(now);
+                List<Posts> all = postsRepository.findAll();
+                assertThat(all.get(0).getTitle()).isEqualTo(updatedTitle);
+                assertThat(all.get(0).getContent()).isEqualTo(updatedContent);
 
-    }
+        }
+
+        @Test
+        public void BaseTimeEntity_등록() {
+                LocalDateTime now = LocalDateTime.of(2022, 2, 8, 0, 0, 0);
+                postsRepository.save(Posts.builder().title("title").content("content").build());
+
+                List<Posts> postsList = postsRepository.findAll();
+
+                Posts posts = postsList.get(0);
+                assertThat(posts.getCreatedData()).isAfter(now);
+                assertThat(posts.getModifiedDate()).isAfter(now);
+
+        }
+
+        private List<Posts> createPosts(int postsNum) {
+                String baseTitle = "title";
+                String baseContent = "content";
+                List<Posts> result = new ArrayList<>();
+                for (int i = 0; i < postsNum; i++) {
+                        result.add(Posts.builder()
+                                        .title(baseTitle + Integer.toString(i))
+                                        .content(baseContent + Integer.toString(i))
+                                        .author(testMember)
+                                        .build());
+                }
+                postsRepository.saveAll(result);
+                return result;
+        }
 }
