592cafa720188ea872465b69a7ed7c2ad5673d1e
ParkJiHo <iks15174@daum.net>
2022-03-04 12:03:43 +0900

    Comments 등록 테스트 추가

--- src/test/java/com/jiho/board/springbootaws/web/CommentsApiControllerTest.java
+++ src/test/java/com/jiho/board/springbootaws/web/CommentsApiControllerTest.java
@@ -1,24 +1,49 @@
 package com.jiho.board.springbootaws.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jiho.board.springbootaws.domain.comments.Comments;
 import com.jiho.board.springbootaws.domain.comments.CommentsRepository;
+import com.jiho.board.springbootaws.domain.member.Member;
+import com.jiho.board.springbootaws.domain.member.MemberRepository;
+import com.jiho.board.springbootaws.domain.member.Social;
 import com.jiho.board.springbootaws.domain.posts.Posts;
 import com.jiho.board.springbootaws.domain.posts.PostsRepository;
+import com.jiho.board.springbootaws.util.security.WithMockCustomUser;
 import com.jiho.board.springbootaws.web.dto.comments.CommentsSaveRequestDto;
+import com.jiho.board.springbootaws.web.dto.member.MemberSaveRequestDto;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CommentsApiControllerTest {
+
+    private Member testMember;
+
+    private MockMvc mvc;
 
     @LocalServerPort
     private int port;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private WebApplicationContext context;
 
     @Autowired
     private CommentsRepository commentsRepository;
@@ -26,17 +51,59 @@ public class CommentsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+        memberRepository.deleteAll();
+        commentsRepository.deleteAll();
+    }
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+        // WithMockCustomUser의 정보와 일치하는 유저 생성
+        testMember = memberRepository.save(MemberSaveRequestDto.builder()
+                .email("testUser@test.com")
+                .password("1234")
+                .name("testUser")
+                .social(Social.NoSocial).build()
+                .toEntity(passwordEncoder));
+    }
+
     @Test
+    @WithMockCustomUser
     public void Comments_등록된다() throws Exception {
         Posts testPosts = postsRepository
-                .save(Posts.builder().title("title").content("content").author("author").build());
+                .save(Posts.builder().title("title").content("content").build());
 
         String content = "comment content";
-        String author = "author";
         Long postsId = testPosts.getId();
 
-        CommentsSaveRequestDto requestDto = CommentsSaveRequestDto.builder().content(content).author(author)
+        CommentsSaveRequestDto requestDto = CommentsSaveRequestDto.builder()
+                .content(content)
                 .postsId(postsId).build();
+
+        String url = "http://localhost:" + port + "/api/v1/comments";
+
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+        List<Comments> all = commentsRepository.findAll();
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+        assertThat(all.get(0).getAuthor().getId()).isEqualTo(testMember.getId());
+        assertThat(all.get(0).getPosts().getId()).isEqualTo(postsId);
 
     }
 }
