9f2938ba7ca66950218ad43bf3bbb068b6cef3a2
ParkJiHo <iks15174@daum.net>
2022-03-05 17:05:03 +0900

    commentRepository findByPosts_Id 추가

--- src/main/java/com/jiho/board/springbootaws/domain/comments/CommentsRepository.java
+++ src/main/java/com/jiho/board/springbootaws/domain/comments/CommentsRepository.java
@@ -1,7 +1,9 @@
 package com.jiho.board.springbootaws.domain.comments;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
-
+    Page<Comments> findByPosts_Id(Long postId, Pageable pagealbe);
 }
