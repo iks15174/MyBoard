0bf1661a72f349e930967194ef0beba1af074f89
ParkJiHo <iks15174@daum.net>
2022-03-05 17:01:36 +0900

    BaseTimeEntity createData -> createDate 변경

--- src/main/java/com/jiho/board/springbootaws/domain/BaseTimeEntity.java
+++ src/main/java/com/jiho/board/springbootaws/domain/BaseTimeEntity.java
@@ -17,7 +17,7 @@ import lombok.Getter;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    private LocalDateTime createdData;
+    private LocalDateTime createdDate;
 
     @LastModifiedDate
     private LocalDateTime modifiedDate;
